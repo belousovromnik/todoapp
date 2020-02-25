9b2155f9b1265088404d641f11f4f35b04e171dc
belousov.romnik@yandex.ru <belousov.romnik@yandex.ru>
2020-02-25 10:31:34 +0300

    Empty django app

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -111,9 +111,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-
 # STATIC_URL = '/static/'
 
 STATIC_URL = '/asset-v1:SkillFactory+PWS-1+5JUN2019+type@asset+block@/'
@@ -121,46 +118,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 django_heroku.settings(locals())
 
-
-# def get_cache():
-#     environment_ready = all(
-#         os.environ.get(f'MEMCACHIER_{key}', False)
-#         for key in ['SERVERS', 'USERNAME', 'PASSWORD']
-#     )
-#     if not environment_ready:
-#         cache = {'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'}
-#     else:
-#         servers = os.environ['MEMCACHIER_SERVERS']
-#         username = os.environ['MEMCACHIER_USERNAME']
-#         password = os.environ['MEMCACHIER_PASSWORD']
-#         cache = {
-#             'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
-#             'TIMEOUT': 300,
-#             'LOCATION': servers,
-#             'OPTIONS': {
-#                 'binary': True,
-#                 'username': username,
-#                 'password': password,
-#                 'behaviors': {
-#                     # Enable faster IO
-#                     'no_block': True,
-#                     'tcp_nodelay': True,
-#                     # Keep connection alive
-#                     'tcp_keepalive': True,
-#                     # Timeout settings
-#                     'connect_timeout': 2000,  # ms
-#                     'send_timeout': 750 * 1000,  # us
-#                     'receive_timeout': 750 * 1000,  # us
-#                     '_poll_timeout': 2000,  # ms
-#                     # Better failover
-#                     'ketama': True,
-#                     'remove_failed': 1,
-#                     'retry_timeout': 2,
-#                     'dead_timeout': 30,
-#                 }
-#             }
-#         }
-#     return {'default': cache}
 
 def get_cache():
     import os
@@ -191,14 +148,4 @@ def get_cache():
 
 
 CACHES = get_cache()
-# CACHES = {
-#     'default': {
-#         'BACKEND': 'django_bmemcached.memcached.BMemcached',
-#         'LOCATION': os.environ.get('MEMCACHEDCLOUD_SERVERS').split(','),
-#         'OPTIONS': {
-#             'username': os.environ.get('MEMCACHEDCLOUD_USERNAME'),
-#             'password': os.environ.get('MEMCACHEDCLOUD_PASSWORD')
-#         }
-#     }
-# }
 SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
